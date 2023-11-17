284ecd99ff572ddda66bb6f53874507a9c4ba25d
naphat <naphat.chan@kkumail.com>
2023-11-17 11:06:45 +0700

    setup project

--- Naphat.java
+++
@@ -1,5 +0,0 @@
-Public Class Naphat {
-    public static void main (String[] args) {
-        System.out.println("Heelo World");
-    }
-}
---
+++ lab1/Naphat.java
@@ -0,0 +1,6 @@
+package lab1;
+public class Naphat {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}

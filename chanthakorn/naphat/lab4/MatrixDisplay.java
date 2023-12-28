f188f65cfa64ddd202d27ef4eea21a9597ba21c3
naphat <naphat.chan@kkumail.com>
2023-12-28 15:48:49 +0700

    edit variables & upload outputs

--- chanthakorn/naphat/lab4/MatrixDisplay.java
+++ chanthakorn/naphat/lab4/MatrixDisplay.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class MatrixDisplay {
 
     private static int[][] matrix;
-    private static int row;
-    private static int col;
+    private static int rowDimension;
+    private static int columnDimension;
 
     public static void main(String[] args) {
         inputMatrix();
@@ -18,19 +18,19 @@ public class MatrixDisplay {
 
         do {
             System.out.print("Enter the number of rows: ");
-            row = scanner.nextInt();
+            rowDimension = scanner.nextInt();
             System.out.print("Enter the number of columns: ");
-            col = scanner.nextInt();
+            columnDimension = scanner.nextInt();
 
-            if (row <= 0 || col <= 0) {
-                System.out.println("Both rows and columns must be greater than 0. Please try again.");
+            if (rowDimension <= 0 || columnDimension <= 0) {
+                System.err.println("Both rows and columns must be greater than 0. Please try again.");
             }
-        } while (row <= 0 || col <= 0);
+        } while (rowDimension <= 0 || columnDimension <= 0);
 
-        matrix = new int[row][col];
+        matrix = new int[rowDimension][columnDimension];
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for (int i = 0; i < rowDimension; i++) {
+            for (int j = 0; j < columnDimension; j++) {
                 System.out.print("Enter element [" + (i + 1) + "][" + (j + 1) + "]: ");
                 matrix[i][j] = scanner.nextInt();
             }
@@ -41,8 +41,8 @@ public class MatrixDisplay {
     public static void displayMatrix() {
         System.out.println("Displaying Matrix:");
 
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
+        for (int i = 0; i < rowDimension; i++) {
+            for (int j = 0; j < columnDimension; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
